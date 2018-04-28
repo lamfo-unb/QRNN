d8c63f7faee34ac22108911830d0324a0165b7b2
Stefano Dantas <34652852+sgdantas@users.noreply.github.com>
2018-04-28 17:11:38 -0300

    Benchmark from R file (#11)
    * added benchmark
    
    * commented code
    
    * corrections
    
    * add adaptive test
    
    * small changes

--- src/qrnn/models.py
+++ src/qrnn/models.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 from toolz.curried import *
 import tensorflow as tf
+import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from keras.optimizers import Adam
-
+from qrnn.evaluation import quantile_loss_evaluator
 
 # TODO : Implement recurrent variational dropout
 
@@ -47,3 +48,37 @@ def qrnn_learner(dataset, price_cols, target_col, prediction_col="prediction",
         return new_dataset.assign(**{prediction_col: model.predict(x_new)})
 
     return p, p(dataset)
+
+def adaptive(dataset,price_cols, target_col, prediction_col, tau):  
+
+    
+    from scipy.stats import norm
+    from scipy.optimize import brent
+    #initializing VaR arrays
+    
+    VaR_train = np.zeros(len(dataset))
+    
+    VaR_train[0] = - norm.ppf(tau) * np.std(dataset[price_cols])
+
+
+    def adaptative(beta):
+        for i in range(1,len(dataset)):
+            VaR_train[i] = VaR_train[i-1] + beta*((dataset[price_cols].iloc[i-1] < - VaR_train[i-1]) - tau ) 
+
+        VaRdataset = dataset.assign(**{prediction_col:VaR_train})
+        return quantile_loss_evaluator(VaRdataset, price_cols, prediction_col,tau)  
+
+   #beta that minimizes loss   
+    beta_opt = brent(adaptative, brack=(-100,100))
+
+
+    def p(new_dataset):
+        VaR = np.zeros(len(new_dataset))
+        VaR[0] = - norm.ppf(tau) * np.std(new_dataset[price_cols])
+        for i in range(1,len(new_dataset)):
+            VaR[i] = VaR[i-1] + beta_opt*((new_dataset[price_cols].iloc[i-1] < - VaR[i-1]) - tau ) 
+        
+        return new_dataset.assign(**{prediction_col:VaR})
+        
+
+    return p,p(dataset)
--- tests/test_models.py
+++ tests/test_models.py
@@ -32,3 +32,33 @@ def test_qrnn_learner():
     # check if model is making a prediction column
     assert Counter(expected_col_train) == Counter(pred_train.columns.tolist())
     assert Counter(expected_col_test) == Counter(pred_test.columns.tolist())
+
+def test_adaptive():
+    from qrnn.models import adaptive
+
+    train = pd.DataFrame({
+        'date': pd.to_datetime(["2017-01-02", "2017-01-03", "2017-01-04", "2017-01-06"]),
+        'price1': [1.980263, -8.167803, 2.16421, -2.13451],
+        'y': [4.094501, -3.509012, 5.50190, -1.51501],
+    })
+
+    test = pd.DataFrame({
+        'date': pd.to_datetime(["2017-01-07", "2017-01-08", "2017-01-09", "2017-01-10"]),
+        'price1': [2.403210, -2.403153, 6.43115, -1.43201],
+        'price2': [4.910245, -1.470514, 8.40329, -2.57012],
+    })
+
+    model, pred_train = adaptive(dataset=train,
+                                     price_cols="price1",
+                                     target_col="y",
+                                     prediction_col="AdaptiveVaR",tau=0.04)
+
+    pred_test = model(test)
+
+    expected_col_train = train.columns.tolist() + ["AdaptiveVaR"]
+    expected_col_test = test.columns.tolist() + ["AdaptiveVaR"]
+
+    # check if model is making a prediction column
+    assert Counter(expected_col_train) == Counter(pred_train.columns.tolist())
+    assert Counter(expected_col_test) == Counter(pred_test.columns.tolist())
+
